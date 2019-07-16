635cef21e8c54867d70a03b867d6f0b8b1647106
hdn <hieudn5293@yahoo.de>
2019-07-16 12:27:52 +0200

    Copy client part of aurelia vscode
    Add some console.logs to show change is okay

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -49,7 +49,7 @@ export function activate(context: ExtensionContext) {
 	// Create the language client and start the client.
 	client = new LanguageClient(
 		'languageServerExample',
-		'Language Server Example',
+		'ahtsaht Language Server Example',
 		serverOptions,
 		clientOptions
 	);
