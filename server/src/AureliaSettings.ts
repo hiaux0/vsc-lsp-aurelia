0cd77c774be024c91a4323e3749c08f005a9018a
hdn <hieudn5293@yahoo.de>
2019-07-17 21:32:39 +0200

    Copy server

---
+++ server/src/AureliaSettings.ts
@@ -0,0 +1,11 @@
+export default class AureliaSettings {
+  public quote: string = '"';
+  public validation: boolean = true;
+  public bindings = {
+    data : []
+  }
+
+  public featureToggles = {
+    smartAutocomplete : true
+  }
+}
---
+++ server/src/CodeActions/OneWayBindingDeprecatedCodeAction.ts
@@ -0,0 +1,17 @@
+import { Diagnostic, TextEdit, Command, TextDocument } from "vscode-languageserver";
+
+export class OneWayBindingDeprecatedCodeAction {
+  public name = 'aurelia-binding-one-way-deprecated';
+
+  public async commands(diagnostic: Diagnostic, document: TextDocument): Promise<Command> {
+
+    return Command.create(
+      `Change 'one-way' binding behaviour to 'to-view'`, 
+      'aurelia-binding-one-way-deprecated', 
+      document.uri, 
+      document.version, 
+      [
+        TextEdit.replace(diagnostic.range, 'to-view')
+      ]);
+  }
+}
